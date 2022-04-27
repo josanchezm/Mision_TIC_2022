73646893ba22970cd97f210a2b8b866549ff88b3
Jaime Sanchez <jimisanchezm@hotmail.com>
2022-04-27 16:46:06 -0500

    primera version

---
+++ ejercicio1.py
@@ -0,0 +1,76 @@
+# comentar una linea de codigo
+
+# print("holaaaaa")
+
+'''
+print("Algoritmo Adquisicion_libro")
+print("Inicio")
+print("\t 1. Saber cual libro se quiere adquirir")
+print("\t 2. Dirigirse a una libreria")
+print("\t 3. Preguntar por el libro")
+
+print("\t Si esta disponible")
+print("\t \t Comprarlo y detenerse ahi")
+
+print("\t si no esta disponible")
+print("\t \t volver al paso 2")
+'''
+'''
+a = 8 
+b = a 
+c = a+b
+
+print(a,b,c)
+
+var = "asdsada"
+print(var)
+
+var = 1.7
+print(var)
+
+var += 1.7
+print(var)
+'''
+'''
+x = 1
+x = x*2
+x *= 2
+print(x)
+'''
+
+'''
+rem = 5 
+rem = rem % 10
+print(rem)
+rem %= 10
+print(rem)
+'''
+
+'''
+j = 50 
+x = 100 
+var = 55 
+rem = 99 
+i = 65 
+j =  j - (i+var+rem)
+print(j)
+j -= (i+var+rem)
+x = x**2 
+print(x)
+x **= 2
+print(j,x)
+'''
+
+# errores en declaracion de variables
+
+'''
+var 1 = 120 
+@var = 120 
+1var = 120
+var-1 = 120
+'''
+
+# asignacion multiple en variables
+
+var1,var2,var3 = 1,2,3
+print(var1,var2,var3)
